9bc6d2bb8f458dfd3bfd4e094cb034e94a5e9afa
Andra Muntean <andramuntean@Andras-MacBook-Air.local>
2021-03-13 17:02:45 +0200

    SM-2 Added the students Table and some dummy student list

--- src/loose/oose/fis/lab/student/manager/Main.java
+++ src/loose/oose/fis/lab/student/manager/Main.java
@@ -1,20 +1,22 @@
-package sample;
+package loose.oose.fis.lab.student.manager;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+//import loose.oose.fis.lab.student.manager.fxml.view-students;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("loose.oose.fis.lab.student.manager.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/view-students.fxml"));
+        primaryStage.setTitle("Student Manager");
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
+
 
 
     public static void main(String[] args) {
